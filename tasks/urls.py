03ede8a7e6031deb4b7b3a6910b1633352c8fa2d
JoaoPedro <joaopedro.r4mos@gmail.com>
2020-05-13 18:24:03 -0300

    Nova funcionalidade: Usuário pode adicionar tarefas no banco de dados

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('helloworld/', views.helloworld),
     path('', views.taskList, name='task-list'),
+    path('task/<int:id>', views.taskView),
+    path('newtask/', views.newTask),
     path('yourname/<str:name>', views.yourname)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def taskList(requests):
+    tasks = Task.objects.all().order_by('-create_at')
+    return render(requests, 'task/list.html', {'tasks': tasks})
 
+def taskView(requests, id):
+    task = get_object_or_404(Task, pk=id)
+    data = {
+        'task': task
+    }
+    return render(requests, 'task/task.html', data)
+
+def newTask(requests):
+    if requests.method == 'POST':
+        form = TaskForm(requests.POST)
+
+        if form.is_valid():
+            task = form.save(commit= False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        data = {
+            'form': form
+        }
+        return render(requests, 'task/addtask.html', data)
 
 def helloworld(requests):
     return HttpResponse('Hello World')
-
-def taskList(requests):
-    return render(requests, 'task/list.html')
 
 def yourname(requests, name):
     return render(requests, 'task/yourname.html', {'name': name})
