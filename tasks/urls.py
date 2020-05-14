72b14b5b788153564f67cfcfbf083dd63a752dc8
JoaoPedro <joaopedro.r4mos@gmail.com>
2020-05-14 19:10:47 -0300

    Finalizando o CRUD do projeto

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.taskList, name='task-list'),
     path('task/<int:id>', views.taskView),
     path('newtask/', views.newTask),
+    path('edit/<int:id>', views.editTask),
+    path('delete/<int:id>', views.deleteTask),
     path('yourname/<str:name>', views.yourname)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 def taskList(requests):
     tasks = Task.objects.all().order_by('-create_at')
@@ -29,6 +30,36 @@ def newTask(requests):
             'form': form
         }
         return render(requests, 'task/addtask.html', data)
+
+def editTask(requests, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+    data = {
+        'form': form,
+        'task': task
+    }
+    if requests.method == 'POST': 
+        form = TaskForm(requests.POST, instance=task)
+
+        if form.is_valid():
+            task.save()
+            print('válido')
+            return redirect('/')
+        else:
+            print('inválido')
+            return render(requests, 'task/edittask.html', data)
+    else:
+        print('Não é o método POST')
+        return render(requests, 'task/edittask.html', data)
+
+def deleteTask(requests, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(requests, 'Tarefa deletada com sucesso!')
+
+    return redirect('/')
+    
 
 def helloworld(requests):
     return HttpResponse('Hello World')
