5a7794798d9b0f98640a55e49ed232dccc1b5d30
JoaoPedro <joaopedro.r4mos@gmail.com>
2020-05-18 01:25:31 -0300

    .

--- curso/settings.py
+++ curso/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'tasks',
     'about',
     'users',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
